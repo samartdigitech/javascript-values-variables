052f4dcea124dae9c1dcf80ccf4d5f7f2628db2c
SAMART NUANPAKSEE <admin@samartdigitech.com>
2026-02-19 17:42:14 +0700

    fix: anser the quation ex 1and2 ex 3 ex 4

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,14 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstName = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const nameRestaurant = "Chef's Table by Chef James";
+const totalAmount = 15000;
+const totalAvalableRestaurent = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+totalAmount = 5000;
+totalAvalableRestaurent = 2500;
